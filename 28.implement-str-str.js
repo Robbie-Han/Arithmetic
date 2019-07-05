618c83d887567ded5dd7da0ef77a33c41e9abb51
USTC-Han <839057430@qq.com>
2019-07-05 21:38:03 +0800

    add remove-element and str marching

---
+++ 28.implement-str-str.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ *
+ * https://leetcode.com/problems/implement-strstr/description/
+ *
+ * algorithms
+ * Easy (32.24%)
+ * Total Accepted:    447.2K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '"hello"\n"ll"'
+ *
+ * Implement strStr().
+ * 
+ * Return the index of the first occurrence of needle in haystack, or -1 if
+ * needle is not part of haystack.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "aaaaa", needle = "bba"
+ * Output: -1
+ * 
+ * 
+ * Clarification:
+ * 
+ * What should we return when needle is an empty string? This is a great
+ * question to ask during an interview.
+ * 
+ * For the purpose of this problem, we will return 0 when needle is an empty
+ * string. This is consistent to C's strstr() and Java's indexOf().
+ * 
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+/使用字符串String.prototype.search(RegExp),有的话返回索引，没有的话返回-1./
+var strStr = function(haystack, needle) {
+  var reg = new RegExp(needle);
+  return haystack.search(reg)
+};
+
