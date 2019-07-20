ba5073350178204f1201565812a3282b370b6143
USTC-Han <839057430@qq.com>
2019-07-20 22:43:20 +0800

    update 7-20

---
+++ 171.excel-sheet-column-number.js
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=171 lang=javascript
+ *
+ * [171] Excel Sheet Column Number
+ *
+ * https://leetcode.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (51.71%)
+ * Total Accepted:    228.3K
+ * Total Submissions: 440K
+ * Testcase Example:  '"A"'
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A"
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "AB"
+ * Output: 28
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "ZY"
+ * Output: 701
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    let e = s.length;
+    let sum = 0;
+    for(let item of s){
+      sum += Math.pow(26,e-1) * (item.charCodeAt() - 64);
+      e--;
+    }
+    return sum
+};
+
